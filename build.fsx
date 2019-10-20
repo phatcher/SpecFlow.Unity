8821f147a608f593d92da4e1f404ccb2a36a1249
Arnaud Tamaillon <arnaud.dev@gmail.com>
2019-10-20 19:14:15 +0200

    Update build scripts to FAKE 5

--- build.fsx
+++ build.fsx
@@ -1,12 +1,14 @@
 /// FAKE Build script
 
 #r "packages/build/FAKE/tools/FakeLib.dll"
-open Fake
-open Fake.AssemblyInfoFile
-open Fake.Git
-open Fake.NuGetHelper
-open Fake.ReleaseNotesHelper
-open Fake.Testing.NUnit3
+open Fake.Core
+open Fake.Core.TargetOperators
+open Fake.DotNet
+open Fake.DotNet.Testing
+open Fake.IO
+open Fake.IO.FileSystemOperators
+open Fake.IO.Globbing.Operators
+open Fake.Tools
 
 // Version info
 let projectName = "SpecFlow.Unity"
@@ -14,55 +16,52 @@ let projectSummary = ""
 let projectDescription = "SpecFlow plugin for using Unity as a dependency injection framework for step definitions."
 let authors = ["Paul Hatcher"]
 
-let release = LoadReleaseNotes "RELEASE_NOTES.md"
+let release = ReleaseNotes.load "RELEASE_NOTES.md"
 
 // Properties
 let buildDir = "./build"
-let toolsDir = getBuildParamOrDefault "tools" "packages/build"
+let toolsDir = Environment.environVarOrDefault "tools" "packages/build"
 let solutionFile = "SpecFlow.Unity.sln"
 
 let nunitPath = toolsDir @@ "/NUnit.ConsoleRunner/tools/nunit3-console.exe"
 
 // Targets
-Target "Clean" (fun _ ->
-    CleanDirs [buildDir;]
+Target.create "Clean" (fun _ ->
+    Shell.cleanDirs [buildDir;]
 )
 
-Target "PackageRestore" (fun _ ->
-    !! solutionFile
-    |> MSBuildRelease buildDir "Restore"
-    |> Log "AppBuild-Output: "
-)
-
-Target "SetVersion" (fun _ ->
+Target.create "SetVersion" (fun _ ->
     let commitHash = 
         try 
-            Information.getCurrentHash()
+            Git.Information.getCurrentHash()
         with
             | ex -> printfn "Exception! (%s)" (ex.Message); ""
     let infoVersion = String.concat " " [release.AssemblyVersion; commitHash]
-    CreateCSharpAssemblyInfo "./code/SolutionInfo.cs"
-        [Attribute.Version release.AssemblyVersion
-         Attribute.FileVersion release.AssemblyVersion
-         Attribute.InformationalVersion infoVersion]
+    AssemblyInfoFile.createCSharp "./code/SolutionInfo.cs"
+        [AssemblyInfo.Version release.AssemblyVersion
+         AssemblyInfo.FileVersion release.AssemblyVersion
+         AssemblyInfo.InformationalVersion infoVersion]
 )
 
-Target "Build" (fun _ ->
-    !! solutionFile
-    |> MSBuild buildDir "Build" 
-        [
-            "Configuration", "Release"
-            "Platform", "Any CPU"
-            "PackageVersion", release.AssemblyVersion
-            "PackageReleaseNotes", release.Notes |> toLines
-            "IncludeSymbols", "true"
-        ]
-    |> Log "AppBuild-Output: "
+Target.create "Build" (fun _ ->
+    let setParams (defaults:MSBuildParams) =
+        { defaults with
+            DoRestore = true
+            Properties =
+                [
+                    "Platform", "Any CPU"
+                    "PackageVersion", release.AssemblyVersion
+                    "PackageReleaseNotes", release.Notes |> String.toLines
+                    "IncludeSymbols", "true"
+                ]
+         }
+    MSBuild.runRelease setParams buildDir "Build" [solutionFile]
+    |> Trace.logItems "AppBuild-Output: "
 )
 
-Target "Test" (fun _ ->
+Target.create "Test" (fun _ ->
     !! (buildDir + "/*.Test.dll")
-    |> NUnit3 (fun p ->
+    |> NUnit3.run (fun p ->
        {p with
           ToolPath = nunitPath
           // Oddity as this creates a build directory in the build directory
@@ -70,21 +69,20 @@ Target "Test" (fun _ ->
           ShadowCopy = false})
 )
 
-Target "Release" (fun _ ->
+Target.create "Release" (fun _ ->
     let tag = String.concat "" ["v"; release.AssemblyVersion] 
-    Branches.tag "" tag
-    Branches.pushTag "" "origin" tag
+    Git.Branches.tag "" tag
+    Git.Branches.pushTag "" "origin" tag
 )
 
-Target "Default" DoNothing
+Target.create "Default" ignore
 
 // Dependencies
 "Clean"
     ==> "SetVersion"
-    ==> "PackageRestore"
     ==> "Build"
     // ==> "Test"
     ==> "Default"
     ==> "Release"
 
-RunTargetOrDefault "Default"
+Target.runOrDefault "Default"
